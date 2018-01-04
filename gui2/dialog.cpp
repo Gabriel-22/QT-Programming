1798e92b46d3de3184e3b98a6f8ac9ceb948d6c7
Gabriel <gabrielfatto216@gmail.com>
2018-01-04 17:07:27 -0200

    spliter and gui tutorials

--- gui2/dialog.cpp
+++ gui2/dialog.cpp
@@ -1,5 +1,8 @@
 #include "dialog.h"
 #include "ui_dialog.h"
+#include <QtCore>
+#include <QtGui>
+#include <QMessageBox>
 
 Dialog::Dialog(QWidget *parent) :
     QDialog(parent),
@@ -11,4 +14,9 @@ Dialog::Dialog(QWidget *parent) :
 Dialog::~Dialog()
 {
     delete ui;
+}
+
+void Dialog::on_pushButton_clicked()
+{
+    QMessageBox::information(this,"Title here","Hello World");
 }
