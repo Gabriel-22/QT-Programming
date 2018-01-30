959006ebcc242cbcfc285a6f1708448a3fa2362a
Gabriel <gabrielfatto216@gmail.com>
2018-01-30 15:27:06 -0200

    Add example of pointers

--- Introduction4/main.cpp
+++ Introduction4/main.cpp
@@ -6,6 +6,18 @@ using namespace std;
 
 int main(){
 
+    int variable = 20;
+    int *pointer;
+
+    pointer = &variable;
+
+    cout << "value of variable: " << variable << endl;
+    cout << "adress of variable: " << &variable << endl;
+    cout << endl;
+    cout << "value stored in pointer: " << *pointer << endl;
+    cout << "adress stored in pointer: " << pointer << endl;
+
+
     Example ob1,ob2;
     ob1.typeSomething();
     ob2.typeSomething();
